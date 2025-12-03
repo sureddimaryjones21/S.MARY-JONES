5363a81eaa35edcf42788308e083b268a5e0630d
sureddimaryjones21 <sureddimaryjones@gmail.com>
2025-12-04 00:08:20 +0530

    Add files via upload

---
+++ abudant.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int N, i;
+    scanf("%d", &N);
+
+    for (i = 1; i <= N; i++) {
+        if (N % i == 0) {   // Check if i divides N exactly
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
+
