728a0102b040ef2e820165ec41fc9a7e612fa616
sureddimaryjones21 <sureddimaryjones@gmail.com>
2025-12-04 18:15:23 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+const float pi=3.14;
+int r;
+scanf("%d",&r);
+ float areaofcircle =pi*r*r;
+printf("areaofcircle is:%f\n",areaofcircle);
+float areaofcircumference=2*pi*r;
+printf("areaofcircle is:%f\n",areaofcircumference);
+return 0;
+}
